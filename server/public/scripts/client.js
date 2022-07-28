dc8563eee9d0afd1865df0474673ca5f4be97424
Andrew Kane <andrew.kane@gmail.com>
2022-07-28 14:42:39 -0500

    made html, transfer functions, some interface features

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,60 @@
 $(document).ready(handleReady);
 
 function handleReady() {
+  retrieveGuesses();
   console.log("jquery is loaded!")
+  $('#submit').on('click', submitGuess);
+}
+
+let round = 0;
+
+function submitGuess() {
+  round++;
+  console.log('in /POST');
+  $.ajax({
+    method: 'POST',
+    url: '/guesses',
+    data: {
+      Justin: $('#justinIn').val(),
+      Kelsey: $('#kelseyIn').val(),
+      Andrew: $('#andrewIn').val(),
+      Madeline: $('#madelineIn').val(),
+      round: round
+    }
+  }).then(function(response){
+    console.log(response);
+    retrieveGuesses();
+  })
+  $('#justinIn').val('')
+  $('#kelseyIn').val('')
+  $('#andrewIn').val('')
+  $('#madelineIn').val('')
+}
+
+function retrieveGuesses() {
+  console.log('in /GET');
+  $.ajax({
+    method: 'GET',
+    url: '/guesses'
+  }).then(function(response) {
+    console.log(response); // should be the array of guess objects
+    displayGuesses(response);
+  })
+}
+
+function displayGuesses(guesses) {
+  console.log(guesses);
+
+  $('#guessTable').empty();
+
+  for(let guess of guesses){
+  $('#guessTable').append(`
+  <tr>
+    <td>${guess.round}</td>
+    <td>${guess.Justin}</td>
+    <td>${guess.Kelsey}</td>
+    <td>${guess.Andrew}</td>
+    <td>${guess.Madeline}</td>
+  </tr>
+  `)}
 }
--- server/server.js
+++ server/server.js
@@ -9,8 +9,26 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// GET & POST Routes go here
+function generateRN() {  // Random Number Generator
+  let RNG = 0;
+  RNG = Math.floor(Math.random() * 25 ) + 1;
+  return RNG;
+}
 
+
+
+let guessArray = [{Justin: 4, Kelsey: 2, Andrew: 12, Madeline: 1}]; // dummy data
+
+// GET & POST Routes go here
+app.get('/guesses', (req, res) => {
+  // res.sendStatus(200);
+  res.send(guessArray);
+})
+
+app.post('/guesses', (req, res) => {
+  guessArray.push(req.body)
+  res.sendStatus(200);
+})
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
